cb50553945c1c773eb4b1e5017363b66fe66ce49
rick <rg.henriques@hotmail.com>
2023-12-11 18:50:54 +0000

    manage expenses and context

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,7 +1,78 @@
-import { Text } from "react-native";
+import { useLayoutEffect } from "react";
+import { StyleSheet, Text, View, ViewBase } from "react-native";
 
-function ManageExpense() {
-  return <Text>Manage expenses</Text>;
+import IconButton from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/styles";
+import Button from "../components/UI/Button";
+
+function ManageExpense({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId; //route prop comes from components loaded as a screen
+  const isEditing = !!editedExpenseId; //adding !! in front of a value turns it into boolean (truthy / falsey)
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
+  function deleteExpenseHandler() {
+    navigation.goBack();
+  }
+
+  function cancelHandler() {
+    navigation.goBack();
+  }
+
+  function confirmHandler() {
+    navigation.goBack();
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? "Update" : "Add"}
+        </Button>
+      </View>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+});
---
+++ store/expenses-context.js
@@ -0,0 +1,186 @@
+import { createContext, useReducer } from "react";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "shoes",
+    amount: 39.99,
+    date: new Date("2023-02-15"),
+  },
+  {
+    id: "e2",
+    description: "jacket",
+    amount: 79.95,
+    date: new Date("2023-04-23"),
+  },
+  {
+    id: "e3",
+    description: "backpack",
+    amount: 49.99,
+    date: new Date("2023-06-08"),
+  },
+  {
+    id: "e4",
+    description: "hat",
+    amount: 19.99,
+    date: new Date("2023-08-12"),
+  },
+  {
+    id: "e5",
+    description: "gloves",
+    amount: 14.95,
+    date: new Date("2023-09-30"),
+  },
+  {
+    id: "e6",
+    description: "t-shirt",
+    amount: 24.99,
+    date: new Date("2023-10-15"),
+  },
+  {
+    id: "e7",
+    description: "jeans",
+    amount: 59.99,
+    date: new Date("2023-12-02"),
+  },
+  {
+    id: "e8",
+    description: "umbrella",
+    amount: 12.99,
+    date: new Date("2023-01-21"),
+  },
+  {
+    id: "e9",
+    description: "sunglasses",
+    amount: 29.99,
+    date: new Date("2023-03-18"),
+  },
+  {
+    id: "e10",
+    description: "scarf",
+    amount: 18.5,
+    date: new Date("2023-05-07"),
+  },
+  {
+    id: "e11",
+    description: "watch",
+    amount: 89.99,
+    date: new Date("2023-07-09"),
+  },
+  {
+    id: "e12",
+    description: "socks",
+    amount: 9.99,
+    date: new Date("2023-09-14"),
+  },
+  {
+    id: "e13",
+    description: "hoodie",
+    amount: 49.99,
+    date: new Date("2023-10-28"),
+  },
+  {
+    id: "e14",
+    description: "wallet",
+    amount: 29.95,
+    date: new Date("2023-02-03"),
+  },
+  {
+    id: "e15",
+    description: "belt",
+    amount: 15.99,
+    date: new Date("2023-04-17"),
+  },
+  {
+    id: "e16",
+    description: "water bottle",
+    amount: 8.99,
+    date: new Date("2023-06-22"),
+  },
+  {
+    id: "e17",
+    description: "earphones",
+    amount: 34.99,
+    date: new Date("2023-08-05"),
+  },
+  {
+    id: "e18",
+    description: "notebook",
+    amount: 7.5,
+    date: new Date("2023-09-19"),
+  },
+  {
+    id: "e19",
+    description: "candle",
+    amount: 12.49,
+    date: new Date("2023-11-11"),
+  },
+  {
+    id: "e20",
+    description: "mug",
+    amount: 9.99,
+    date: new Date("2023-12-30"),
+  },
+];
+
+export const ExpensesContext = createContext({
+  expenses: [],
+  addExpense: ({ description, amount, date }) => {},
+  deleteExpense: (id) => {},
+  updateExpense: (id, { description, amount, date }) => {},
+});
+
+function expensesReducer(state, action) {
+  switch (action.type) {
+    case "ADD":
+      const id = new Date().toString() + Math.random().toString();
+      return [{ ...action.payload, id: id }, ...state];
+    case "UPDATE":
+      const updatableExpenseIndex = state.findIndex(
+        (expense) => expense.id === action.payload.id
+      );
+
+      const updatableExpense = state[updatableExpenseIndex];
+      const updatedItem = { ...updatableExpense, ...action.payload.data }; // spreading out values where their index already exists will result in an update of those values
+
+      const updatedExpenses = [...state];
+      updatedExpenses[updatableExpenseIndex] = updatedItem;
+
+      return updatedExpenses;
+    case "DELETE":
+      return state.filter((expense) => expense.id !== action.payload);
+    default:
+      return state;
+  }
+}
+
+function ExpensesContextProvider({ children }) {
+  const [expensesState, dispatch] = useReducer(expensesReducer, DUMMY_EXPENSES);
+
+  function addExpense({ expenseData }) {
+    dispatch({ type: "ADD", payload: expenseData });
+  }
+
+  function updateExpense({ expenseData }) {
+    dispatch({ type: "UPDATE", payload: { id: id, data: expenseData } });
+  }
+
+  function deleteExpense({ expenseData }) {
+    dispatch({ type: "DELETE", payload: id });
+  }
+
+  const value = {
+    expenses: expensesState,
+    addExpense: addExpense,
+    updateExpense: updateExpense,
+    deleteExpense: deleteExpense,
+  };
+
+  return (
+    <ExpensesContext.Provider value={value}>
+      {children}
+    </ExpensesContext.Provider>
+  );
+}
+
+export default ExpensesContextProvider;
